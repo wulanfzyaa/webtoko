79aeba539f87ebb023ed9d6018477ec249edb8ae
wulanfawwazia-afk <wulanfawwazia@gmail.com>
2026-06-09 23:18:50 +0700

    Pratikum 9 - CRUD input produk

--- resources/views/list_produk.blade.php
+++ resources/views/list_produk.blade.php
@@ -32,6 +32,27 @@
             </tbody>
         </table>
     </div>
+    <div><h1>Input Produk</h1></div>
+<form method="POST" action="{{ route('produk.simpan') }}">
+    @csrf
+    <table class="table">
+        <tr>
+            <td>Nama:</td>
+            <td colspan="3"><input type="text" class="form-control" id="nama" name="nama"></td>
+        </tr>
+        <tr>
+            <td>Deskripsi:</td>
+            <td colspan="3"><textarea class="form-control" id="deskripsi" name="deskripsi"></textarea></td>
+        </tr>
+        <tr>
+            <td>Harga:</td>
+            <td><input type="number" class="form-control" id="harga" name="harga"></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
+    <button type="submit" class="btn btn-primary">Simpan</button>
+</form>
 </div>
 
 </body>
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ListProdukController;
 
 Route::get('/listproduk', [ListProdukController::class, 'show']);
+Route::post('/listproduk', [ListProdukController::class, 'simpan'])->name('produk.simpan');
